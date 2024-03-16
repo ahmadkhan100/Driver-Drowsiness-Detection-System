6234efc11261c4ee33897726ac8046cba3325239
ahmadkhan100 <163046609+ahmadkhan100@users.noreply.github.com>
2024-03-16 02:08:49 -0400

    Update alert_system.py

--- alert_system.py
+++ alert_system.py
@@ -1,1 +1,21 @@
+# alert_system.py
+import os
+from pygame import mixer  # Used to play sound
+
+# Initialize the mixer module
+mixer.init()
+
+def play_sound(sound_file):
+    """Play sound from the given sound file."""
+    mixer.music.load(sound_file)
+    mixer.music.play()
+
+def drowsiness_alert():
+    """Play a drowsiness alert sound."""
+    alert_sound = 'alert.wav'  # Replace with the path to your alert sound file
+    play_sound(alert_sound)
+
+# If you have a more complex alert system, you could add more functions here
+
+
 
